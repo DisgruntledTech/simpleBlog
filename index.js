1ca4b8c33aa799ce92d6de6bd36cbc004d4b1d96
DisgruntledTech <82328363+DisgruntledTech@users.noreply.github.com>
2022-08-01 20:50:03 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//Lightweight server option
+
+const http = require('http')
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  console.log(req.headers)
+  console.log(req.headers.cookie)
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain',)
+  res.end('Hello Big New World\n')
+  
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
