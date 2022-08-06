d80485f55f70286983b9479b91c2534cddb178dd
DisgruntledTech <82328363+DisgruntledTech@users.noreply.github.com>
2022-08-06 14:51:25 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 //Lightweight server option
+//https://nodejs.dev/learn/nodejs-file-paths
 
 const http = require('http')
+const path = require('path');
+const fs = require('fs');
+const controller = require('./controller')
 
 const hostname = '127.0.0.1'
 const port = 3000
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers)
-  console.log(req.headers.cookie)
+  //console.log(req.headers)
+  //console.log(req.headers.cookie)
+  //const responcedoc = controller.index();
+  switch(path.basename){
+    case "":
+      //do something
+      break;
+  }
+
   res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain',)
-  res.end('Hello Big New World\n')
+  res.setHeader('Content-Type', 'text/html',)
+  
+  fs.readFile("./Views/index.html", 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+      data = data.replace("<p>blog loop</p>","Worked");
+      res.end(data);      
+    });    
   
 })
 
