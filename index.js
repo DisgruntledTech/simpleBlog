34d1d8ec95299fc018c5ae4ae61ca0e6a2d3c47f
DisgruntledTech <82328363+DisgruntledTech@users.noreply.github.com>
2025-07-29 20:24:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,116 +7,78 @@ const fs = require('fs');
 const url = require('url');
 
 const controller = require('./controller');
-const { Console } = require('console');
 
 const hostname = '127.0.0.1'
 const port = 3000
 
-
 const server = http.createServer((req, res) => {
- 
-  var body = "";
-  req.on('data', function (chunk) {
+  let body = "";
+  req.on('data', chunk => {
     body += chunk;
   });
-  req.on('end', function () {
-    
-    if(req.method === 'GET'){
-      const rPath =req.url.toString().toLowerCase();
-      const jPath = `Posts/${path.basename(rPath)}`;
+  req.on('end', () => {
+    if (req.method === 'GET') {
+      const rPath = req.url.toString().toLowerCase();
+      const fileName = path.basename(rPath);
+      const jPath = `Posts/${fileName}`;
 
-      switch(path.basename(rPath)){
-        case "":
-          fs.readFile("Views/index.html", 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-                res.statusCode = 404
-                res.setHeader('Content-Type', 'text/html')  
-                res.end("<h1>Index not found!</h1>")
-            }
-              res.statusCode = 200
-              res.setHeader('Content-Type', 'text/html')          
-              res.end(data);      
-            });  
-          break;
-    
-        case "index":
-          fs.readFile("Views/index.html", 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-                res.statusCode = 404
-                res.setHeader('Content-Type', 'text/html')  
-                res.end("<h1>Index not found!</h1>")
-            }
-              res.statusCode = 200
-              res.setHeader('Content-Type', 'text/html')
-              res.end(data);      
-            });  
-          break;
-    
-          case "index.html":
-            fs.readFile("Views/index.html", 'utf8', (err, data) => {
-              if (err) {
-                  console.error(err);
-                  res.statusCode = 404
-                  res.setHeader('Content-Type', 'text/html')  
-                  res.end("<h1>Index not found!</h1>")
-              }
-                res.statusCode = 200
-                res.setHeader('Content-Type', 'text/html')
-                res.end(data);      
-              });  
-            break;
-    
-        case "favicon.ico":
-          fs.readFile("favicon.ico", 'utf-8', (err, data) => {
-            if (err) {
-                console.error(err);
-                res.statusCode = 404
-                res.setHeader('Content-Type', 'text/html')  
-                res.end("<h1>Index not found!</h1>")
-            }
-              res.statusCode = 200
-              res.setHeader('Content-Type', 'image/x-icon')
-              //fs.createReadStream(FAVICON).pipe(res);
-              //return;                   
-              res.end(data);
-            });
-          break;
-    
-        default:
-          //all other request types
-          fs.readFile(jPath, 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-                res.statusCode = 404
-                res.setHeader('Content-Type', 'text/html')  
-                res.end("<h1>Index not found!</h1>")
-                return;
-            }
-              res.statusCode = 200
-              res.setHeader('Content-Type', 'utf-8')
-              res.end(data);      
-            });  
-          break;
+      // Group index file cases
+      if (fileName === "" || fileName === "index" || fileName === "index.html") {
+        fs.readFile("Views/index.html", 'utf8', (err, data) => {
+          if (err) {
+            console.error(err);
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end("<h1>Index not found!</h1>");
+            return;
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          res.end(data);
+        });
+      } else if (fileName === "favicon.ico") {
+        fs.readFile("favicon.ico", (err, data) => {
+          if (err) {
+            console.error(err);
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end("<h1>Favicon not found!</h1>");
+            return;
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'image/x-icon');
+          res.end(data);
+        });
+      } else {
+        // all other request types (posts)
+        fs.readFile(jPath, 'utf8', (err, data) => {
+          if (err) {
+            console.error(err);
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end("<h1>Post not found!</h1>");
+            return;
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          res.end(data);
+        });
       }
-    }
-    else if(req.method === 'POST'){    
-      //What is the form name?
-      //Pass the form to the controller function
-      console.log('POSTed: ' + body);    
-      controller.createPost(body);  
+    } else if (req.method === 'POST') {
+      console.log('POSTed: ' + body);
+      controller.createPost(body);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
       res.end('worked!');
+    } else {
+      console.log('DID NOT WORK!');
+      res.statusCode = 405;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Method Not Allowed');
     }
-    else{
-      Console.log('DID NOT WORK!')
-    }
-    
   });
-      
-
-})
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
-})
+});
